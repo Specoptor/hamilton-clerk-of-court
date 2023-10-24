db8f9466a41b92607012651be8b1076df3fd62a7
faizan <FAIZANNAGHMAN@GMAIL.COM>
2023-10-24 18:28:41 +0500

    use get_processed_pdfs() in playground
    Signed-off-by: faizan <FAIZANNAGHMAN@GMAIL.COM>

--- pdf_extractor_playground.py
+++ pdf_extractor_playground.py
@@ -1,7 +1,7 @@
-from utils import process_directory, extract_datapoints_from_pdf
+from utils import get_processed_pdfs, extract_datapoints_from_pdf
 
 if __name__ == '__main__':
-    list_of_pdfs = process_directory()  # this will take a while, since it runs on the entire directory
+    list_of_pdfs = get_processed_pdfs()  # this will take a while, since it runs on the entire directory
     datapoints_list = {}
     for case_number, pdf_pages_list in list_of_pdfs.items():
         datapoints_list[case_number] = extract_datapoints_from_pdf(pdf_pages_list)
