ca4b152b2aa420856b22aff7de16570e55e5e73f
AbMohiz <abmohiz22@gmail.com>
2023-10-26 01:28:41 +0500

    Add the option to extract data points for a specified case number.

--- regex_sandbox.py
+++ regex_sandbox.py
@@ -1,12 +1,13 @@
 import json
 from utils import extract_datapoints_from_pdf
-
+# from PDF_extractor import extract_property_address
 with open('parsed_pdfs.json', 'r') as f:
     parsed_pdfs = json.load(f)
 
 datapoints_dict = {}
 for case_number, pdf in parsed_pdfs.items():
-    datapoints_dict[case_number] = extract_datapoints_from_pdf(pdf)
+    if case_number == "A2303511":
+        datapoints_dict[case_number] = extract_datapoints_from_pdf(pdf)
 
 with open('datapoints_sandbox.json', 'w') as f:
     json.dump(datapoints_dict, f)
