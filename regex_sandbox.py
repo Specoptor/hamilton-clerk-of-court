c7bbc88662b3026e8a763d462e10e05efe718fee
faizan <FAIZANNAGHMAN@GMAIL.COM>
2023-10-26 04:56:22 +0500

    generate a csv of datapoints extracted and update requirements.txt.
    Signed-off-by: faizan <FAIZANNAGHMAN@GMAIL.COM>

--- regex_sandbox.py
+++ regex_sandbox.py
@@ -1,13 +1,23 @@
 import json
 from utils import extract_datapoints_from_pdf
+import pandas as pd
 # from PDF_extractor import extract_property_address
+
 with open('parsed_pdfs.json', 'r') as f:
     parsed_pdfs = json.load(f)
 
 datapoints_dict = {}
 for case_number, pdf in parsed_pdfs.items():
-    if case_number == "A2303511":
+    # if case_number == "A2303511":
         datapoints_dict[case_number] = extract_datapoints_from_pdf(pdf)
 
 with open('datapoints_sandbox.json', 'w') as f:
     json.dump(datapoints_dict, f)
+
+# add case number to each dict
+for case_number, datapoints in datapoints_dict.items():
+    datapoints['case_number'] = case_number
+
+datapoints_list = datapoints_dict.values()
+df = pd.DataFrame(datapoints_list)
+df.to_csv('datapoints_sandbox.csv', index=False)
