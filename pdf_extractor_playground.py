9a812cc553fe8f6ee0b64ae8e9c0914aed2d718c
faizan <FAIZANNAGHMAN@GMAIL.COM>
2023-10-25 05:31:09 +0500

    dump the extracted datapoints to a json list after processing all the pdfs.
    Signed-off-by: faizan <FAIZANNAGHMAN@GMAIL.COM>

--- pdf_extractor_playground.py
+++ pdf_extractor_playground.py
@@ -1,8 +1,11 @@
 from utils import get_processed_pdfs, extract_datapoints_from_pdf
+import json
 
 if __name__ == '__main__':
     list_of_pdfs = get_processed_pdfs()  # this will take a while, since it runs on the entire directory
     datapoints_list = {}
     for case_number, pdf_pages_list in list_of_pdfs.items():
         datapoints_list[case_number] = extract_datapoints_from_pdf(pdf_pages_list)
-    print(datapoints_list)
+
+    with open('datapoints_list.json', 'w') as f:
+        json.dump(datapoints_list, f, indent=4)
