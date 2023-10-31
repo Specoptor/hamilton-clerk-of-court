2440ee53667a2353d33927a66e6dedad5e55570b
AbMohiz <abmohiz22@gmail.com>
2023-10-31 21:43:33 +0500

    CSV set.

--- regex_sandbox.py
+++ regex_sandbox.py
@@ -8,6 +8,12 @@ with open('parsed_pdfs.json', 'r') as f:
 
 datapoints_dict = {}
 for case_number, pdf in parsed_pdfs.items():
+    # if case_number == "A2303703":
+    # if case_number in [
+    #     'A2303246', 'A2303250', 'A2303447', 'A2303452', 'A2303474', 'A2303475', 'A2303476', 'A2303484', 'A2303494',
+    #     'A2303508', 'A2303512'
+    #
+    # ]:
     datapoints_dict[case_number] = extract_datapoints_from_pdf(pdf)
 
 with open('datapoints_sandbox.json', 'w') as f:
@@ -15,6 +21,7 @@ with open('datapoints_sandbox.json', 'w') as f:
 
 # add case number to each dict
 for case_number, datapoints in datapoints_dict.items():
+
     datapoints['case_number'] = case_number
 
 datapoints_list = datapoints_dict.values()
