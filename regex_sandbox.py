8fb8d30e21f50cfab5571f59ed1a8681248ecde0
AbMohiz <abmohiz22@gmail.com>
2023-11-02 20:21:31 +0500

    Updated JSON and CSV with the Price regex to skip $0 values.

--- regex_sandbox.py
+++ regex_sandbox.py
@@ -8,13 +8,12 @@ with open('parsed_pdfs.json', 'r') as f:
 
 datapoints_dict = {}
 for case_number, pdf in parsed_pdfs.items():
-    # if case_number == "A2303703":
-    # if case_number in [
-    #     'A2303246', 'A2303250', 'A2303447', 'A2303452', 'A2303474', 'A2303475', 'A2303476', 'A2303484', 'A2303494',
-    #     'A2303508', 'A2303512'
-    #
-    # ]:
-    datapoints_dict[case_number] = extract_datapoints_from_pdf(pdf)
+    # if case_number == "A2303257":
+    if case_number in [
+        'A2303249', 'A2303257', 'A2303258', 'A2303259', 'A2303260', 'A2303272', 'A2303278'
+
+    ]:
+        datapoints_dict[case_number] = extract_datapoints_from_pdf(pdf)
 
 with open('datapoints_sandbox.json', 'w') as f:
     json.dump(datapoints_dict, f)
